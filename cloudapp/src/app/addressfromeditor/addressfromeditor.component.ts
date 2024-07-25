74c06862b1df8aa5601a10fbcb977ee7fc0b0289
cedelis <cedelis@uillinois.edu>
2024-07-25 13:32:46 -0500

    Alma Letter labels and translations
    My previous commit was an attempt at allowing for language translations. It didn't work because I hadn't realized
    that Alma treats English as the default 'Description' value for labels.
    
    Here is my second attempt at it
    
    Here are my most recent observations:
    
      * letter labels metadata are gathered from /conf/letters
    
      * letter labels themselves are stored in /conf/code-tables/xxx (as
    referenced from labels.link in metadata), e.g., /conf/code-tables/ResetPwLetter
    
      * letter label Descriptions are always stored in the English language
    code-table, e.g., /conf/code-tables/ResetPwLetter?lang=en
    
      * letter label Translations are stored in the language-specific
    code-table, e.g., German (de), /conf/code-tables/ResetPwLetter?lang=de
    
      * if you wish to update a non-English label (i.e., create a
    Translation), you must update *both* the English code-table
    (Description) and *also* the language-specific code-table (Translation)
    
      * It seems that the most recent API call using the 'lang' parameter
    adversely affects the Alma session. E.g., if your Alma session is
    in German and the most recent (final) API call used was 'lang=es',
    then when you leave the Cloud App and return to Alma, you will
    notice that web pages will begin to be translated into this most
    recent language, e.g., Spanish (however, I notice that the action
    buttons aren't affected). I believe I've found a work-around for
    this which is to make sure that the Cloud App always makes an API
    call using a blank value for 'lang', e.g.,
    /conf/code-tables/ResetPwLetter?lang=
    This seems to 'reset' Alma back to its original language (e.g., German)

--- cloudapp/src/app/addressfromeditor/addressfromeditor.component.ts
+++ cloudapp/src/app/addressfromeditor/addressfromeditor.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit, Injectable } from '@angular/core';
 import { CloudAppRestService, CloudAppEventsService, RestErrorResponse, AlertService, Request, HttpMethod } from '@exlibris/exl-cloudapp-angular-lib';
-import { map, catchError, switchMap, tap, concatMap, toArray, filter, finalize } from 'rxjs/operators';
+import { map, catchError, switchMap, mergeMap, tap, toArray, filter, finalize, take } from 'rxjs/operators';
 import { CanActivate, Router, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
 import { of, from, forkJoin } from 'rxjs';
 import { AppService } from '../app.service';
@@ -18,6 +18,7 @@ export class AddressFromEditorComponent implements OnInit {
   letters?: Letter[];
   num = 100;
   loading = false;
+  languagesLoaded = false;
   processed = 0;
   showProgress = false;
   dirtyControls: { [key: string]: number } = {};
@@ -30,7 +31,9 @@ export class AddressFromEditorComponent implements OnInit {
   labelLinks = new Map<string, string>();
 
   languages = new Array<string>();
-  selectedLanguage?: string;
+  letterMapByLanguage = new Map<string , Map<string, Letter>>();
+  selectedLanguage = 'en'; // default
+  selectedLanguages = new Array<string>();
 
   constructor( 
     private restService: CloudAppRestService,
@@ -39,30 +42,75 @@ export class AddressFromEditorComponent implements OnInit {
   ) { }
 
   ngOnInit() {
+    this.loadLanguages();
+  }
+
+  languageChanged(newVal, lang) {
+    console.log(`selectedLanguage: ${this.selectedLanguage}`);
+    this.populateSelectedLanguages();
+    this.clearLetters();
+  }
+  
+  // Hack alert: When we make an API call using the 'lang' parameter, Alma seems to get confused and begins
+  // to translate future Alma pages using the most recent language used in the 'lang' parameter value.
+  // Making a final random API call with an empty 'lang' parameter seems to remedy this
+  resetToDefaultLanguage() {
+    let firstLabelLink = this.labelLinks.values().next().value;
+    let url = firstLabelLink + this.languageParameter(''); 
+    //console.log(`languageReset: ${firstLabelLink} , url: ${url}`);
+    this.restService.call(url).subscribe({
+        next: (s: any)=>{
+          //console.log(`resetToDefaultLanguage() call finished: ${JSON.stringify(s)}`);
+        }
+    });
+  }
+
+  // for scalability reasons (Alma API calls will timeout if > 30 sec),
+  // we can only process up to two languages at a time
+  // with English necessarily being one of them.
+  // This is because English is treated as the 'base' language, whereas
+  // all other languages are considered 'translations'.
+  populateSelectedLanguages() {
+    this.selectedLanguages = new Array<string>();
+    this.selectedLanguages.push('en');
+    // English language is always necessary in Alma apparently
+    if (this.selectedLanguage != 'en') 
+      this.selectedLanguages.push(this.selectedLanguage);
+    console.log(`selectedLanguages: ${this.selectedLanguages} (${this.selectedLanguages.length})`);
+  }
+
+  loadLanguages() {
     this.restService.call('/conf/mapping-tables/InstitutionLanguages')
     .pipe(
-      concatMap((l: any) => l.row ),
+      mergeMap((l: any) => l.row ),
       filter((l: any) => l.enabled),
       toArray(),
-    ).subscribe(
-        {
+    ).subscribe({
         next: (s: any[])=>{
           s.forEach(lang=>{
             if (isRestErrorResponse(lang)) {
               console.log(`Error retrieving languages: ${lang.message}`);
             } else {
+              // since English code-table is always necessary, no need to keep track of it
               console.log(`Loaded language: ${lang.column0}`);
-              this.selectedLanguage = lang.column0;
-              this.languages.push(lang.column0);
+              if (lang.column0 != 'en') {
+                this.selectedLanguage = lang.column0; // just set default as the most recent one encountered
+                this.languages.push(lang.column0);
+              }
             }
-          })
+          });
+          this.populateSelectedLanguages();
         },
-        error: e => this.alert.error('Error in loading languages(): ', e.message),
+        error: e => this.alert.error('Error in loadLanguages(): ', e.message),
         complete: () => {
+          this.letterMapByLanguage.set('en', new Map<string, Letter>());
+          this.languages.forEach(l=>{
+            this.letterMapByLanguage.set(l, new Map<string, Letter>());
+          });
           console.log('Finished loading languages')
-          console.log(`Current language: ${this.selectedLanguage}`);
+          this.languagesLoaded = true;
         },
-        }
+      }
     );
   }
 
@@ -72,48 +120,71 @@ export class AddressFromEditorComponent implements OnInit {
     this.processed = 0;
     this.showProgress = false;
     this.num = 0;
-    let url = '/conf/letters' + this.languageParameter();
+    let url = '/conf/letters';
     this.restService.call(url)
     .pipe(
-      concatMap((l: any) => l.letter ),
+      mergeMap((l: any) => l.letter ),
       filter((l: any) => l.enabled.value==='true'),
       filter((l: any) => l.channel==='EMAIL'),
-      tap(() => this.num++),
+      tap(() => this.num+=this.selectedLanguages.length),
       tap((l: any) => {
         this.letterDescriptions.set(l.code, l.name);
         this.labelLinks.set(l.code, l.labels.link);
       }),
-      map((l: any) => this.getLetter(l)),
+      //take(10), // for debugging, only work with small subset
+      mergeMap((l: any) => this.getLetter(l)),
       toArray(),
       tap(() => this.showProgress = true),
       switchMap(reqs => forkJoin(reqs)),
-      finalize(() => this.loading = false),
-    ).subscribe(
-        {
-        next: (s: any[])=>{
+      finalize(() => {this.loading = false; this.resetToDefaultLanguage()}),
+    ).subscribe({
+        next: (s: any)=>{
+          let keepGoing = true;
           s.forEach(letter=>{
-            if (isRestErrorResponse(letter)) {
-              console.log(`Error retrieving letter: ${letter.message}`);
-            } else {
-              let l: Letter = new Letter(letter);
-              l.description = this.letterDescriptions.get(l.name);
-              this.letters.push(l);
+            if (keepGoing) {
+              if (isRestErrorResponse(letter)) {
+                console.log(`Error retrieving letter: ${letter.message}`);
+                keepGoing = false;
+              } else {
+                let l: Letter = new Letter(letter);
+                let letterMap: Map<string, Letter> = this.letterMapByLanguage.get(letter.language.value);
+                console.log(`caching ${letter.language.value} : ${letter.description}`);
+                letterMap.set(l.name, l);
+
+                //////////////
+                // if multiple languages are enabled in Alma, there will be multiple versions of each letter
+                // populate only one of each letter into letters[]
+                let found = this.letters.findIndex((obj) => {
+                  return obj.name === l.name;
+                });
+                if (found === -1) {
+                  l.description = this.letterDescriptions.get(l.name);
+                  this.letters.push(l);
+                }
+                //////////////
+              }
             }
-          })
+          });
+
         },
         error: e => this.alert.error('Error in loadLetters(): ', e.message),
         complete: () => this.sortLetters(),
-        }
-    );
+        });
   }
 
   getLetter(letter: any) {
-    let url = this.labelLinks.get(letter.code) + this.languageParameter();
-    return this.restService.call(url).pipe(
-      tap(() => this.processed++ ),
-      tap(() => console.log(`getting ${letter.description}`)),
-      catchError(e => of(e)),
-    )
+    const observableArray = [];
+    this.selectedLanguages.forEach(lang=>{
+      let url = this.labelLinks.get(letter.code) + this.languageParameter(lang); 
+      observableArray.push(
+        this.restService.call(url).pipe(
+          tap(() => this.processed++ ),
+          tap(() => console.log(`getting ${letter.description} ; lang ${lang}`)),
+          catchError(e => of(e)),
+        )
+      );
+    });
+    return observableArray;
   }
 
   updateLetters() {
@@ -124,11 +195,11 @@ export class AddressFromEditorComponent implements OnInit {
     this.num = 0;
     from(this.getUpdatedLetters())
     .pipe(
-      map(l=>this.updateLetter(l)),
+      mergeMap((l: any) => this.updateLetter(l)),
       toArray(),
       tap(() => this.showProgress = true),
       switchMap(reqs=>forkJoin(reqs)),
-      finalize(() => this.loading = false),
+      finalize(() => {this.loading = false; this.resetToDefaultLanguage()}),
     )
     .subscribe({
       next: (s: any[]) => {
@@ -149,7 +220,7 @@ export class AddressFromEditorComponent implements OnInit {
               console.log(`refreshed cached Letter object: ${newLetter.description}`)
             }
           }
-        })
+        });
       },
       error: e => this.alert.error('Error in updateLetters(): ', e.message),
     });
@@ -160,7 +231,7 @@ export class AddressFromEditorComponent implements OnInit {
     this.num = 0;
     this.letters.forEach(l=> {
       if (l.isDirty()) {
-        this.num++;
+        this.num+=this.selectedLanguages.length;
         updatedLetters.push(l);
       }
     })
@@ -171,19 +242,31 @@ export class AddressFromEditorComponent implements OnInit {
   }
 
   updateLetter(letter: Letter) {
-    let url =  this.labelLinks.get(letter.name) + this.languageParameter(); 
-    letter.setAddressFrom(letter.addressFrom, letter.addressFromEnabled);
-    const requestBody = letter.restObject;
-    let request: Request = {
-      url: url,
-      method: HttpMethod.PUT,
-      requestBody
-    };
-    return this.restService.call(request).pipe(
-      tap(() => this.processed++ ),
-      tap(() => console.log(`updating ${letter.description}`)),
-      catchError(e => of(e)),
-    )
+    const observableArray = [];
+    this.selectedLanguages.forEach(lang=>{
+      let letterMap: Map<string, Letter> = this.letterMapByLanguage.get(lang);
+      let thisLetter: Letter = letterMap.get(letter.name);
+
+      let url =  this.labelLinks.get(letter.name) + this.languageParameter(lang); 
+      thisLetter.setAddressFrom(letter.addressFrom, letter.addressFromEnabled);
+      const requestBody = thisLetter.restObject;
+      //console.log(`lang:${lang} requestBody: ${JSON.stringify(requestBody)}`);
+      let request: Request = {
+        url: url,
+        method: HttpMethod.PUT,
+        requestBody
+      };
+      observableArray.push(
+        this.restService.call(request).pipe(
+          tap(() => this.processed++ ),
+          tap(() => console.log(`updating ${letter.description}`)),
+          catchError(e => of(e)),
+        )
+      );
+    });
+
+    return observableArray;
+
   }
 
   clearLetters() {
@@ -197,14 +280,9 @@ export class AddressFromEditorComponent implements OnInit {
     return Object.keys(this.dirtyControls).length;
   }
 
-  languageChanged(newVal, lang) {
-    console.log(`selectedLanguage: ${this.selectedLanguage}`);
-    this.clearLetters();
-  }
-
   // helper
-  languageParameter() {
-    return (this.selectedLanguage ? ('?lang=' + this.selectedLanguage) : '');
+  languageParameter(lang) {
+    return ('?lang=' + lang);
   }
 
   addressFromEnabledChanged(newVal, letter) {
